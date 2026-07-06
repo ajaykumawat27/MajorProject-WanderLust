1fcab86f6b3a231171d5e3287e3446bcb3b014c2
ajaykumawat27 <ajudotnkt@gmail.com>
2026-07-06 19:22:23 +0530

    enable mongo session store and fix production login cookie

--- app.js
+++ app.js
@@ -70,7 +70,6 @@ const sessionOptions = {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,//1 week
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production' 
     }
 };
 
